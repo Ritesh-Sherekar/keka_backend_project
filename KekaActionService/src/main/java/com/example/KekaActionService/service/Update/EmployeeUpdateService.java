21d85b997fdd68ebe0f2db44c149228f27e1d92f
1211-abhishek <abhishekghogare.eidiko@gmail.com>
2025-09-25 12:36:16 +0530

    Added Department entity

--- KekaActionService/src/main/java/com/example/KekaActionService/service/Update/EmployeeUpdateService.java
+++ KekaActionService/src/main/java/com/example/KekaActionService/service/Update/EmployeeUpdateService.java
@@ -1,15 +1,23 @@
 package com.example.KekaActionService.service.Update;
 
+import com.example.KekaActionService.entity.Department;
 import com.example.KekaActionService.entity.Employee;
+import com.example.KekaActionService.repository.DepartmentRepo;
 import com.example.KekaActionService.repository.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class EmployeeUpdateService {
+
+    @Autowired
+    private DepartmentRepo departmentRepo;
+
     private final EmployeeRepo employeeRepo;
 
     public EmployeeUpdateService(EmployeeRepo employeeRepo){
@@ -20,6 +28,15 @@ public class EmployeeUpdateService {
     public Employee updateEmployee(int id , Map<String, Object> updates){
         Employee employee = employeeRepo.findById(id).orElseThrow(()-> new RuntimeException("Not Found"));
 
+        Department department;
+
+        if (updates.containsKey("department")){
+
+            department = departmentRepo.findByDepartmentName((String) updates.get("department")).orElseThrow(() -> new RuntimeException("Department not found"));
+        } else {
+            department = null;
+        }
+
         if (employee != null){
             updates.forEach((key, value)->{
                 switch (key){
@@ -28,7 +45,7 @@ public class EmployeeUpdateService {
                     case "email" : employee.setEmail((String) value); break;
                     case "phone" : employee.setPhone((String) value); break;
                     case "designation" : employee.setDesignation((String) value); break;
-                    case "department" : employee.setDepartment((String) value); break;
+                    case "department" : employee.setDepartment(department); break;
                     case "joinDate" : employee.setJoinDate((LocalDate) value); break;
                     case "active" : employee.setActive((Boolean) value); break;
                     case "easyDelete" : employee.setIsDelete((Boolean) value); break;
