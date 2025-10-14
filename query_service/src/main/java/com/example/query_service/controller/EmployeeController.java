4f3d02e26ef461372104e0dcbb38f60eec3d2eca
shubhamthokare18 <shubhamthokare.eidiko@gmail.com>
2025-10-14 12:10:36 +0530

    query_service

--- query_service/src/main/java/com/example/query_service/controller/EmployeeController.java
+++ query_service/src/main/java/com/example/query_service/controller/EmployeeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/QueryGetService")
+@RequestMapping("/employee")
 public class EmployeeController {
 
     @Autowired
